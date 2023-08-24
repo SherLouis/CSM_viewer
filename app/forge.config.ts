0ac1b06d642de0a48225b965b2eeb40e6b07ee84
Louis Harris <lelightning@hotmail.fr>
2023-08-23 21:00:02 -0400

    configured publish and github actions

--- app/forge.config.ts
+++ app/forge.config.ts
@@ -1,10 +1,9 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import { PublisherGithub } from '@electron-forge/publisher-github'
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -12,9 +11,11 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    prune: true,
+
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [new MakerSquirrel({}), new MakerZIP({})],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
@@ -34,6 +35,9 @@ const config: ForgeConfig = {
       },
     }),
   ],
+  publishers: [
+    new PublisherGithub({repository: {name:'CSM_viewer', owner: 'SherLouis'}, draft: true})
+  ]
 };
 
 export default config;
