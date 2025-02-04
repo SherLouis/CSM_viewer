b3d6f8ab00895c1f4fb33c69cf0434910cfa53c8
Louis Harris <lelightning@hotmail.fr>
2025-02-03 19:24:52 -0500

    fix resultDdo

--- app/src/ui/models/ResultDdo.ts
+++ app/src/ui/models/ResultDdo.ts
@@ -1,6 +1,5 @@
 // ddo = data display object
 export type ResultDdo = {
-    newResult: { amplitude_ma_min: number; amplitude_ma_max: number; amplitude_ma_avg: number; frequency_hz: number; frequency_hz_max: number; duration_s: number; duration_s_max: number; implentation_type: string; contact_separation: number; contact_diameter: number; contact_length: number; phase_length: number; phase_type: string }
     id: number,
     roi: {
         side: string,
